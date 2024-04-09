f40016872546eba37d6fef73291a03ee390a0949
RakibHossainGitHub <rakib016477@gmail.com>
2024-04-09 11:44:46 +0600

    commit1

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// preloader
+let preloader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  preloader.style.display = "none";
+});
+
+//active navebar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
