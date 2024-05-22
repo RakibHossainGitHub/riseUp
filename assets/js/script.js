bd3c8037518859d7456a9c6a1c3e64b65508a090
RakibHossainGitHub <rakib016477@gmail.com>
2024-05-22 15:06:27 +0600

    commit4

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,24 @@ navBar.forEach(function (a) {
   });
 });
 
+//contact
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "username",
+    Password: "password",
+    To: "rakibulislam016477@gmail.com",
+    From: document.getElementById("gmail").value,
+    Subject: "riseup message",
+    Body:
+      "Name: " +
+      document.getElementById("firstName").value +
+      document.getElementById("lastName").value +
+      "<br> Email: " +
+      document.getElementById("gmail").value +
+      "<br> Phone no: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value
+  }).then((message) => alert("message sent succesfully"));
+}
